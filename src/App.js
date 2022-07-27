48d19d678e3b6c574181b82d1763fdb02bd3a81a
Mehrdadkml <mehrdadkml92@gmail.com>
2022-07-27 22:59:48 +0430

    set basic react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/header'
+import NotesListPages from "./pages/NotesListPages";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <NotesListPages/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NotesListPages.js
@@ -0,0 +1,26 @@
+import React ,{useState,useEffect}from 'react'
+
+const NotesListPages = () => {
+
+    let[notes,setNotes]=useState([])
+    useEffect(() =>{
+
+    },[])
+
+
+    let getNotes=async() =>{
+        let response=await fetch('http://127.0.0.1:8000/api/notes/')
+        let data=await response.json()
+        console.log('DATA:',data)
+        setNotes(data)
+    }
+  return (
+    <div>
+      notes
+    </div>
+  )
+}
+
+export default NotesListPages
+
+
