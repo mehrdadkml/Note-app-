1cbdff5511ae1b8e06e301403b61c03cb658ac9a
Mehrdadkml <mehrdadkml92@gmail.com>
2022-07-30 19:21:38 +0430

    connect to django

--- src/pages/NotesListPages.js
+++ src/pages/NotesListPages.js
@@ -1,9 +1,10 @@
 import React ,{useState,useEffect}from 'react'
-
+import Listitem from '../components/Listitem'
 const NotesListPages = () => {
 
     let[notes,setNotes]=useState([])
     useEffect(() =>{
+      getNotes()
 
     },[])
 
@@ -11,16 +12,22 @@ const NotesListPages = () => {
     let getNotes=async() =>{
         let response=await fetch('http://127.0.0.1:8000/api/notes/')
         let data=await response.json()
-        console.log('DATA:',data)
+        
         setNotes(data)
     }
   return (
     <div>
-      notes
+      <div className='notes-list'>
+        {notes.map((note,index)=>(
+          <Listitem key={index} note={note}/>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default NotesListPages
+
+
 
 
